38c8d87ad92c9888bdbfb5dda726a3143fbbffcc
afellman <red.kasid@gmail.com>
2020-01-05 14:29:09 -0500

    save

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -204,7 +204,9 @@ function keyPressed(e) {
     console.log("control")
   }
   if (ctrlPressed && key == "s") {
-    scenes.forEach(scene => console.log(JSON.stringify(scene.save())));
+    scenes.forEach(scene => {
+      scene.save()
+    })));
     ctrlPressed = false;
     console.log("save")
   }
