e7ebcf7b75763374219902e335f890241c39e963
afellman <andrewf@mbshighway.com>
2020-01-05 20:40:03 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -739,23 +739,23 @@ class SpinningCircles extends Sketch {
       // if(i > 0) {
       //   line(x, y, prevX, prevY)
       // }
-      if (dist(x, y, topPoint.x, topPoint.y) < this.proximity) {
+      if (this.connecters.top && dist(x, y, topPoint.x, topPoint.y) < this.proximity) {
         stroke(topPoint.color[0], topPoint.color[1], topPoint.color[2], 80)
         line(Math.round(topPoint.x), Math.round(topPoint.y), Math.round(x), Math.round(y))
       }
-      if (dist(x, y, bottomPoint.x, bottomPoint.y) < this.proximity) {
+      if (this.connecters.top && dist(x, y, bottomPoint.x, bottomPoint.y) < this.proximity) {
         stroke(bottomPoint.color[0], bottomPoint.color[1], bottomPoint.color[2], 80)
         line(Math.round(bottomPoint.x), Math.round(bottomPoint.y), Math.round(x), Math.round(y))
       }
       // FOR CONNECTER LINES ON SIDES
-      // if (dist(x, y, rightPoint.x, rightPoint.y) < this.proximity) {
-      //   stroke(rightPoint.color[0], rightPoint.color[1], rightPoint.color[2], 80)
-      //   line(Math.round(rightPoint.x), Math.round(rightPoint.y), Math.round(x), Math.round(y))
-      // }
-      // if (dist(x, y, leftPoint.x, leftPoint.y) < this.proximity) {
-      //   stroke(leftPoint.color[0], leftPoint.color[1], leftPoint.color[2], 80)
-      //   line(Math.round(leftPoint.x), Math.round(leftPoint.y), Math.round(x), Math.round(y))
-      // }
+      if (this.connecters.top && dist(x, y, rightPoint.x, rightPoint.y) < this.proximity) {
+        stroke(rightPoint.color[0], rightPoint.color[1], rightPoint.color[2], 80)
+        line(Math.round(rightPoint.x), Math.round(rightPoint.y), Math.round(x), Math.round(y))
+      }
+      if (this.connecters.top && dist(x, y, leftPoint.x, leftPoint.y) < this.proximity) {
+        stroke(leftPoint.color[0], leftPoint.color[1], leftPoint.color[2], 80)
+        line(Math.round(leftPoint.x), Math.round(leftPoint.y), Math.round(x), Math.round(y))
+      }
       // ellipse(Math.round(x), Math.round(y), this.circleSize)
       this.centerPoints[i].draw()
       prevX = x;
