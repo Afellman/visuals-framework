fd9d8b377211fb1262bdb4f5e6159e97abc7dd60
afellman <a.fellman88@gmail.com>
2020-01-06 21:13:02 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1483,7 +1483,7 @@ class Drops extends Sketch {
       for (let j = 0; j < this.resolution; j++) {
         thisPoint = this.grid[i][j];
         let acc = p5.Vector.sub(thisPoint, createVector(width / 2, height / 2));
-        thisPoint.add(acc.mult(sin(frameCount / 100) * this.speed));
+        thisPoint.add(acc);
         ellipse(thisPoint.x, thisPoint.y, 2);
       }
     }
