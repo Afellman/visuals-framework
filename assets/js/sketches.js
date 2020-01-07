5d5c99a853cc003e2db152813c4c4e641d8bed37
afellman <a.fellman88@gmail.com>
2020-01-06 21:49:04 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1484,7 +1484,7 @@ class Drops extends Sketch {
       for (let j = 0; j < this.resolution; j++) {
         thisPoint = this.grid[i][j];
         wave.mult(sin(frameCount / 100) / 20)
-        let acc = p5.Vector.sub(thisPoint, this.center);
+        let acc = p5.Vector.sub(thisPoint, createVector(width / 2, height / 2));
         acc.normalize();
         acc.div(frameCount / 100)
         thisPoint.add(acc);
