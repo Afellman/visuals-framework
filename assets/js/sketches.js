1bfbe70b3116f5f3da374e955f48a1750ab3b3d9
afellman <a.fellman88@gmail.com>
2020-01-06 21:28:45 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1485,7 +1485,9 @@ class Drops extends Sketch {
         thisPoint = this.grid[i][j];
         lastPoint = this.grid[i][j - 1];
         let acc = p5.Vector.sub(thisPoint, createVector(width / 2, height / 2));
-        thisPoint.add(acc.normalize().mult(sin(frameCount / 100).mult(dist(this.center, thisPoint))));
+        acc.normalize();
+        acc.mult(dist(this.center, thisPoint))
+        thisPoint.add(acc)
         ellipse(thisPoint.x, thisPoint.y, 2);
         if (j > 0) {
           line(lastPoint.x, lastPoint.y, thisPoint.x, thisPoint.y);
