57f988d8f003e89510a9beca8dec29e2476f5584
afellman <a.fellman88@gmail.com>
2020-01-07 22:11:12 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1334,7 +1334,7 @@ class LinesShader extends Sketch {
   draw() {
     // linesShader.setUniform("u_color", [0.0, 1.0, 0.0, 1.0]) // Get this equation correct.
     noStroke();
-    linesShader.setUniform('u_time', this.waveSpeed)
+    linesShader.setUniform('u_time', frameCount * this.waveSpeed)
     linesShader.setUniform("tex0", this.img);
     linesShader.setUniform("u_loops", this.loops);
     linesShader.setUniform("u_freq", this.freq);
@@ -1342,7 +1342,6 @@ class LinesShader extends Sketch {
     linesShader.setUniform("u_xOff", this.xOff);
     linesShader.setUniform("u_yOff", this.yOff);
     linesShader.setUniform("u_amp", this.amp);
-    linesShader.setUniform("u_waveSpeed", this.waveSpeed);
 
     this.shaderBox.shader(linesShader);
     image(this.shaderBox, 0, 0); // Creating an image from the shader graphics onto the main canvas.
