7bfcf9594f6d03b5f3ae0a069d256bb97868fcfd
afellman <a.fellman88@gmail.com>
2020-01-12 01:06:51 -0500

    save

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -286,7 +286,7 @@ const controlScene = {
           this.isActive = true;
         }
       } else {
-        this.scene.opacity = midiToColor(vel);
+        this.scene.opacity = midiToNormal(vel);
       }
     }
   }
