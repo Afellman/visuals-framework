e2981a55d7772c8b000214a455aa840d9962f3f6
afellman <a.fellman88@gmail.com>
2020-01-12 20:18:38 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -948,7 +948,7 @@ class Starry extends Sketch {
       }
       let size = dist(thisPoint.pos.x, thisPoint.pos.y, width / 2, height / 2) * (this.params.size / 100);
       let acc = p5.Vector.sub(thisPoint.pos, createVector(width / 2, height / 2));
-      thisPoint.pos.add(acc.div(400 - this.params.speed))
+      thisPoint.pos.add(acc.normalize() * 10)
       fill(this.params.color * thisPoint.color, this.params.color * thisPoint.color, this.params.color * thisPoint.color, this.opacity);
       ellipse(thisPoint.pos.x, thisPoint.pos.y, size);
       if (thisPoint.pos.x > width || thisPoint.pos.x < 0 || thisPoint.pos.y > height || thisPoint.pos.y < 0) {
