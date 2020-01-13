e45f19401ed651d6f20de72423a35686207a24f1
afellman <a.fellman88@gmail.com>
2020-01-12 20:11:53 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -946,7 +946,6 @@ class Starry extends Sketch {
       if (thisPoint == undefined) {
         thisPoint = this.addPoint();
       }
-      rotate(PI / 4)
       let size = dist(thisPoint.pos.x, thisPoint.pos.y, width / 2, height / 2) * (this.params.size / 100);
       let acc = p5.Vector.sub(thisPoint.pos, createVector(width / 2, height / 2));
       thisPoint.pos.add(acc.div(400 - (this.params.speed * 10)))
