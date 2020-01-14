6c6708984143a01c7da68f067d71ba33f8b1ad17
afellman <a.fellman88@gmail.com>
2020-01-13 21:06:33 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -191,6 +191,7 @@ class Sketch {
     for (let i = 0; i < this.listeners.length; i++) {
       const thisSocket = this.listeners[i];
       if (thisSocket.socketName && thisSocket.socketMethod) {
+        console.log(`/${this.sceneNum}/${thisSocket.socketName}`)
         socket.on(`/${this.sceneNum}/${thisSocket.socketName}`, thisSocket.method);
       }
     }
