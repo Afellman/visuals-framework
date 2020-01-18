2af2b3ebb5c55cc20917d0584dbeaf68af518057
afellman <a.fellman88@gmail.com>
2020-01-18 17:07:32 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -520,7 +520,7 @@ class SpinningCircles extends Sketch { // Scene 4. Maped
     }
 
     if (this.multiSin) {
-      this.multiplier += sin(frameCount / 10000) / 100;
+      this.multiplier = 1 + sin(frameCount / 10000) / 100;
     }
     this.freq += speed;
   }
