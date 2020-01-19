4a1f3d2d1a2c8b2bbc9572dcd548d4e5dc34c266
afellman <a.fellman88@gmail.com>
2020-01-18 19:14:41 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -714,7 +714,7 @@ class Sin extends Sketch { // Scene 7
       let n = i * 0.005;
       for (let j = 1; j < 4; j++) {
         const thisFreq = this.params.faders["freq" + j];
-        const thsAmp = this.params.faders["amplitude" + j];
+        const thisAmp = this.params.faders["amplitude" + j];
         y += sin(2 * PI * thisFreq * (i + this.time) + 1) * thisAmp * (1 + noise(n, n));
       }
       vertex(x, y);
