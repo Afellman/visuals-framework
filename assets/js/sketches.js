3fda75f87ea3681254dcf4b92d737d46718d6bad
afellman <a.fellman88@gmail.com>
2020-01-18 19:59:04 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -791,7 +791,7 @@ class Orbitals extends Sketch {// Scene 8
       const sinX = sin(changeX * thisSpinner.speed);
       const cosY = cos(changeY * thisSpinner.speed)
       thisSpinner.pos.x = startX + sinX * this.ampX - (i * this.wobbleX);
-      thisSpinner.pos.y = startY + cosY * this.ampY - (i * this.wobbleY);
+      thisSpinner.pos.y = startY + cosY * (this.ampY - (i * this.wobbleY));
       strokeWeight(thisSpinner.weight);
       stroke(thisSpinner.color[0], thisSpinner.color[1], thisSpinner.color[2], this.opacity)
       point(thisSpinner.pos.x, thisSpinner.pos.y)
