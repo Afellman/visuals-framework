cf7e2eb547497599a7dc301a6f81381ded0866f5
afellman <a.fellman88@gmail.com>
2020-01-20 23:14:10 -0500

    save

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -474,7 +474,7 @@ function loadImages(resolve, reject) {
     loadImage("./assets/images/peter2.jpg"),
     loadImage("./assets/images/leaves.jpg"),
     loadImage("./assets/images/waterfall.jpg"),
-    loadImage("./assets/images/alec/006.jpg")
+    loadImage("./assets/images/alec/img14.jpg")
   ])
     .then(res => resolve(res))
     .catch(res => new Error(res));
