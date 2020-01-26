023f3d4ecb6a94c8aabd9940af8e0ec31661494f
afellman <a.fellman88@gmail.com>
2020-01-26 15:04:07 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -621,7 +621,7 @@ class Geometry extends Sketch { // Scene 5. Maped.
     this.movement += this.params.faders.movement / 1000;
   }
   branch(len) {
-    rect(0, 0, 20, -len);
+    point(0, 0, 20, -len);
     translate(0, -len);
     if (len > 4) {
       push();
