5ba54dc823538e3c3c478ea84b4aacb7ea5df651
afellman <a.fellman88@gmail.com>
2020-01-31 22:36:54 -0500

    save

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ function registerIncoming() {
   glClient.on("updateOsc", (val) => {
     for (let i in val.params) {
       console.log(val, "foo");
-      udpPort.send({ address: `/${val.scene}/${i}`, args: [{ value: 0 }, { value: 0 }] }, remoteIP, 9000)
+      udpPort.send({ address: `/${val.scene}/${i}`, args: val.params[i] }, remoteIP, 9000)
     }
   });
 
