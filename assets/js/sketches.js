50b987824f4a32a8b450ac5ab804d50dd483d1db
afellman <a.fellman88@gmail.com>
2020-01-31 22:06:12 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1114,9 +1114,8 @@ class Feedback extends Sketch { // Scene 13. Maped. Needs work.
     {
       socketName: "xy",
       socketMethod: (val) => {
-        this.params.faders.xOff = val.args[1]
-        this.params.faders.yOff = val.args[0]
-        this.updateOsc();
+        this.params.faders.xOff = val.args[1] / 100
+        this.params.faders.yOff = val.args[0] / 100
       }
     },
   ]
