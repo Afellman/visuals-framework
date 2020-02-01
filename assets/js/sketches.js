aa6866e1fed06563697cd1bee99adc794fd1d694
afellman <a.fellman88@gmail.com>
2020-02-01 09:47:00 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1158,6 +1158,8 @@ class Mirror extends Sketch { // Scene 14. Maped. Needs work.
     this.graph.image(glCanvas, 0, 0)
     this.shader.setUniform("u_opacity", this.opacity)
     this.shader.setUniform("tex0", this.graph);
+    this.shader.setUniform('u_xOff', this.params.faders.xOff);
+    this.shader.setUniform('u_yOff', this.params.faders.yOff);
     this.shaderBox.shader(this.shader);
     image(this.shaderBox, 0, 0); // Creating an image from the shader graphics onto the main canvas.
     this.shaderBox.rect(0, 0, width, height);
