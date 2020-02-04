acbe7f9d744465d0741ac75c495227e13eb84531
afellman <andrewf@mbshighway.com>
2020-02-03 20:41:52 -0500

    save

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -677,7 +677,6 @@ function loadShaders(cb) {
     loadShader("./shaders/texture.vert", "./shaders/trippytwo.frag"),
     loadShader("./shaders/texture.vert", "./shaders/videoShader.frag"),
     loadShader("./shaders/texture.vert", "./shaders/mirror.frag"),
-    loadShader("./shaders/texture.vert", "./shaders/flowField.frag"),
   ])
     .then(res => cb(res))
     .catch(res => new Error(res));
