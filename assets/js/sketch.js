525a58f6d895172708383e8113ffffc47118c17c
afellman <andrewf@mbshighway.com>
2020-02-06 19:43:47 -0500

    save

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -506,14 +506,16 @@ function getMIDIMessage(midiMessage) {
   //   genericMidi[note].method(velocity, command);
   // }
   if(command == 177){
-    if(velocity >= 127){
-      midi177[note] ++;
-    } else if(velocity <= 0){
-      midi177[note] --
-    } else {
+    // if(velocity >= 127){
+    //   midi177[note] ++;
+    // } else if(velocity <= 0){
+    //   midi177[note] --
+    // } else {
        
-    }
-    velocity = midi177[note];
+    // }
+    // velocity = midi177[note];
+    
+    
 
   }
   console.log(velocity, midi177[note])
