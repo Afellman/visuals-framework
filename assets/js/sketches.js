818dd6c61d5ff601460742fac1d15786b37a17c9
afellman <andrewf@mbshighway.com>
2020-02-06 22:55:11 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1249,9 +1249,11 @@ class Gridz extends Sketch {
     super();
     this.scale = 100;
     this.rows = Math.ceil(width / this.scale);
-    this.cols = Math.floor(height / this.scale);
+    this.cols = Math.ceil(height / this.scale);
     this.angle = 0;
     this.time = 0;
+    this.xInterval = this.scale / width;
+    this.yInterval =  this.scale / height;
   }
  
   init() {
@@ -1279,7 +1281,7 @@ class Gridz extends Sketch {
     }
 
     this.graph.image(glCanvas, 0, 0)
-    this.shader.setUniform("u_intervalX",  this.scale / width);
+    this.shader.setUniform("u_intervalX", this.xInterval);
     this.shader.setUniform("u_intervalY",  this.scale / height);
     this.shaderBox.shader(this.shader);
     image(this.shaderBox, 0, 0); // Creating an image from the shader graphics onto the main canvas.
