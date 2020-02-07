b1df3a68520ce41cd8c75d543ec1ac08b6777765
afellman <andrewf@mbshighway.com>
2020-02-06 23:02:02 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1292,7 +1292,7 @@ class Gridz extends Sketch {
     // this.shaderBox.shader(this.shader);
     // image(this.shaderBox, 0, 0); // Creating an image from the shader graphics onto the main canvas.
     // this.shaderBox.rect(0, 0, width, height);
-    this.time+= 0.003
+    this.time+= 0.01
   }
  
   listeners = [{}]
