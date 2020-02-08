c1f8cf6ecd760afc9a03e1ec4948a283114b62d2
afellman <red.kasid@gmail.com>
2020-02-08 15:31:37 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1312,6 +1312,7 @@ class Rainbow extends Sketch {
       let x = width / 2 + Math.sin(-HALF_PI + frameCount / 100) * height / 4;
       let y = height / 2 + -Math.abs(Math.sin(frameCount / 100) * height / 4);
 
+      rotate(frameCount / 100)
       line(x - 10, y - 10, x + 10, y + 10);
     }
   }
