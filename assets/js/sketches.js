3c2d9009a2fdc8ebdce6f1f8d5f66df0116d3d44
afellman <red.kasid@gmail.com>
2020-02-08 14:37:33 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1309,7 +1309,7 @@ class Rainbow extends Sketch {
     for (let i = 0; i < this.lineAmt; i++) {
       const thisLine = this.lines[i];
       stroke(thisLine.color);
-      thisLine.posStart.y = Math.sin(thisLine.posStart) / (width * 2);
+      thisLine.posStart.y = Math.sin(thisLine.posStart.x) / (width * 2);
       line(thisLine.posStart.x, thisLine.posStart.y, thisLine.posEnd.x, thisLine.posEnd.y);
     }
   }
