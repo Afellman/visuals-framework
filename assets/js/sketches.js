6490f96ff80fb2b82116c54bb1e2830768f9e7fb
afellman <red.kasid@gmail.com>
2020-02-08 15:23:52 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1313,8 +1313,8 @@ class Rainbow extends Sketch {
       thisLine.posStart.y = height / 2 + -Math.abs(Math.sin(frameCount / 100) * height / 4);
 
 
-      thisLine.posStart.x = width / 2 + Math.sin(frameCount / 100) * height / 4;
-      thisLine.posStart.y = height / 2 + -Math.abs(Math.sin(frameCount / 100) * height / 4);
+      thisLine.posEnd.x = width / 2 + Math.sin(frameCount / 100) * height / 4;
+      thisLine.posEnd.y = height / 2 + -Math.abs(Math.sin(frameCount / 100) * height / 4);
 
 
       line(thisLine.posStart.x, thisLine.posStart.y, thisLine.posEnd.x, thisLine.posEnd.y);
