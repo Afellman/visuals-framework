7ee4ec0c7b7faea6ec0a20cb44ce01713278bb49
afellman <red.kasid@gmail.com>
2020-02-08 16:45:04 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1305,7 +1305,7 @@ class Rainbow extends Sketch {
   }
 
   draw() {
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < this.lineAmt; i++) {
       const thisLine = this.lines[i];
       thisLine.update();
       thisLine.display();
@@ -1338,12 +1338,18 @@ class Rainbow extends Sketch {
         this.time = this.time % 360;
         if (this.time == 0) {
           this.moving = false;
+          this.move();
         }
       }
     }
 
     move() {
+      let rad = radians(this.time);
+      this.posStart.x = width / 2 + Math.sin(-HALF_PI + rad) * this.parent.arc;
+      this.posStart.y = height / 2 + -Math.abs(Math.sin(rad) * this.parent.arc);
 
+      this.posEnd.x = width / 2 + Math.sin(-HALF_PI + rad) * (this.parent.arc) / this.parent.lineLength;
+      this.posEnd.y = height / 2 + -Math.abs(Math.sin(rad) * (this.parent.arc) / this.parent.lineLength);
     }
 
     display() {
