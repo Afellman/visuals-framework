45661cb8bf4133dc93fa3bc2c12a50b3f37ed611
afellman <red.kasid@gmail.com>
2020-02-08 16:14:43 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1322,7 +1322,7 @@ class Rainbow extends Sketch {
 
       line(thisLine.posStart.x, thisLine.posStart.y, thisLine.posEnd.x, thisLine.posEnd.y);
     }
-    this.time = Math.abs(sin(PI * 2 * 360));
+    this.time = Math.abs(sin(PI * 2 * frameCount));
   }
 
   lineBounce() {
