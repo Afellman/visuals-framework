e1b69a1c445296ff127976055bf1875f32505b6e
afellman <red.kasid@gmail.com>
2020-02-08 14:02:17 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1268,10 +1268,10 @@ class Gridz extends Sketch {
       for (let y = 0; y < this.cols; y++) {
         let yPos = y * this.scale;
         let hue = noise(x, y, this.time) * 150;
-        stroke(100);
+        stroke(50);
         push()
         translate(xPos, yPos)
-        line(0 - this.scale * noise(frameCount / 100), 0 - this.scale * noise(frameCount / 100), 0 + this.scale, 0 + this.scale)
+        line(0 - this.scale * noise(hue), 0 - this.scale * noise(hue), 0 + this.scale, 0 + this.scale)
         line(0 - this.scale, 0 + this.scale, 0 + this.scale, 0 - this.scale)
         // line(0, 0 + this.scale, 0, 0  - this.scale)
         pop();
