4b66072cca9b37b4145d56208f5c2614cbc47eae
afellman <andrewf@mbshighway.com>
2020-02-09 15:21:02 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1228,7 +1228,7 @@ class FlowField extends Sketch {
       let xoff = 0;
       for (let x = 0; x < this.cols; x++) {
         let index = x + y * this.cols;
-        let angle = noise(xoff, yoff, this.zoff) * TWO_PI;
+        let angle = noise(xoff, yoff, this.zoff);
         let v = p5.Vector.fromAngle(angle);
 
         v.setMag(this.mag);
