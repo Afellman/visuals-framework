124f070fc336c3e1bac53762242fe1e7d1e29c9d
afellman <andrewf@mbshighway.com>
2020-02-09 21:36:37 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1530,10 +1530,10 @@ class Rainbow extends Sketch {
     move() {
       let rad = radians(this.time);
       this.posStart.x = width / 2 + Math.sin(-HALF_PI + rad) * 768;
-      this.posStart.y = height / 2 + -Math.abs(Math.sin(rad) * height / 4;
+      this.posStart.y = height / 2 + -Math.abs(Math.sin(rad) * height / 4);
 
-      // this.posEnd.x = width / 3 + Math.sin(-HALF_PI + rad) * (this.parent.arc) / this.parent.lineLength;
-      // this.posEnd.y = height / 2 + -Math.abs(Math.sin(rad) * (this.parent.arc) / this.parent.lineLength);
+      this.posEnd.x = width / 3 + Math.sin(-HALF_PI + rad) * (768) / this.parent.lineLength;
+      this.posEnd.y = height / 2 + -Math.abs(Math.sin(rad) * (height / 4) / this.parent.lineLength);
     }
 
     display() {
