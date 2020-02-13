53777443ce21543326328967965670cf12434b40
afellman <a.fellman88@gmail.com>
2020-02-12 21:08:07 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1039,7 +1039,7 @@ class DisplaceImg extends Sketch { // scene 11. maped
     this.shaderBox.shader(this.shader);
     // send the camera and the two other past frames into the camera feed
     this.shader.setUniform('tex0', this.img);
-    this.shader.setUniform("u_opacity", this.opacity)
+    this.shader.setUniform("u_opacity", this.opacity / 255)
     this.shader.setUniform("u_displaceX", noise(frameCount * this.params.faders.freq) * this.params.faders.amp * this.displaceX);
     this.shader.setUniform("u_displaceY", noise(frameCount * this.params.faders.freq) * this.params.faders.amp * this.displaceY);
     this.shader.setUniform('u_time', frameCount);
