d62d0423d4805a09d7ddb1f514898d83e140edc0
afellman <a.fellman88@gmail.com>
2020-02-22 22:53:09 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1861,7 +1861,7 @@ class AudioReactive extends Sketch {
     let prevY = height / 2;
 
     stroke(255);
-    for (let i = 0; i < spectrum.length; i++) {
+    for (let i = 0; i < spectrum.length / 4; i++) {
       x = map(i, 0, spectrum.length, 0, width);
       y = height - 20 - map(spectrum[i], 0, 255, 0, height / 2);
       line(prevX, prevY, x, y)
