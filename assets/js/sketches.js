4070ab72c2162436bd634d05eda5e6c2689c8f43
afellman <a.fellman88@gmail.com>
2020-02-23 20:50:57 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1877,10 +1877,10 @@ class AudioReactive extends Sketch {
     const high = fft.getEnergy("highMid");
     // stroke(255);
     stroke(bass, mid, high);
-    for (let i = 0; i < spectrum.length / 6; i++) {
+    for (let i = 0; i < spectrum.length; i++) {
 
       // Dividing by 6 only grabs the first sixth of the spectrum (where most of the values will be)
-      x = map(i, 0, spectrum.length / 6, 0, width);
+      x = map(i, 0, spectrum.length, 0, width);
       y = height - 20 - map(spectrum[i], 0, 255, 0, height / 2);
 
 
