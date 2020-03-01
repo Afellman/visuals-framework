21c3d0ab3d6dede6db6bc02b129b550faca61e47
afellman <a.fellman88@gmail.com>
2020-03-01 15:14:31 -0500

    save

--- assets/js/sketches.js
+++ assets/js/sketches.js
@@ -1965,7 +1965,7 @@ class Fractal extends Sketch {
 
   circle(size) {
     ellipse(0, 0, size);
-    if (size > this.params.size / 10) {
+    if (size > 10) {
       push();
       translate(-size, this.params.faders.tiltY);
       this.circle(size / 2);
